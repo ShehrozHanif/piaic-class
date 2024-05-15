e3a5e54702b118488b6cec491de99ec371e742de
Shehroz Hanif <shehrozhanif54@gmail.com>
2024-05-15 03:04:13 -0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,23 @@
+// function findFeb29(currentyear:number):number{
+//     while(true){
+//         if(currentyear % 4 === 0 && currentyear % 100 !==0 || currentyear % 400 === 0){
+//             const isfeb29 = new Date(currentyear,1,29).getDate()===0;
+//             if(isfeb29){
+//                 return (currentyear);
+//             }
+//         }
+//         currentyear++;
+//     };
+// };
+// const feb29 = findFeb29(2029)
+// console.log(feb29)
+// import inquirer from "inquirer"
+// const input = await inquirer.prompt({
+//     name:"",
+//     type: 
+// })
+function sum(num1, num2) {
+    return num1 + num2;
+}
+console.log(sum(2, 2));
+export {};
